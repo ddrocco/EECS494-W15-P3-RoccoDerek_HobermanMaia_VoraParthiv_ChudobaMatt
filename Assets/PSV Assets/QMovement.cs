8a86892e89970a8949723c12bbbcce8af33176c0
Parthiv Vora <psvora@umich.edu>
2015-02-19 23:07:09 -0500

    Q Camera
    The "Q" player's camera movements. Attach to the Q camera object, and
    make sure the main player object is applied to the Target in the
    SmoothFollow script. Make sure to add the required Input Axes.

---
+++ Assets/PSV Assets/QMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QMovement : MonoBehaviour {
+
+	public QPan qpan;
+	public MouseLook qlook;
+	public SmoothFollow qsnap;
+
+	// Use this for initialization
+	void Start () {
+		qpan = this.GetComponent<QPan> ();
+		qlook = this.GetComponent<MouseLook> ();
+		qsnap = this.GetComponent<SmoothFollow> ();
+
+		qpan.enabled = true;
+		qlook.enabled = true;
+		qsnap.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float snapping = Input.GetAxis ("QSnap");
+		if(snapping > 0.5){
+			qpan.enabled = false;
+			qlook.enabled = false;
+			qsnap.enabled = true;
+		} else {
+			qpan.enabled = true;
+			qlook.enabled = true;
+			qsnap.enabled = false;
+		}
+	}
+}
