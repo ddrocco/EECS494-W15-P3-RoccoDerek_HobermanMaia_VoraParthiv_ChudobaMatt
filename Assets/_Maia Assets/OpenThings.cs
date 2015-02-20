ba50b6a3e734875ef5f639bba3e4e22d2c1ce321
Maia Hoberman <maiayh@umich.edu>
2015-02-20 17:28:20 -0500

    we have a level with functioning doors and boxes

---
+++ Assets/_Maia Assets/OpenThings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenThings : MonoBehaviour {
+	public Animator anim;
+	
+	void Awake () {
+		anim = GetComponent<Animator>();
+	}
+		
+	void OnMouseDown () {
+		anim.SetBool("isOpen", !anim.GetBool("isOpen"));
+	}
+}
