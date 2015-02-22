f39f6ac52a708bcfb7f84e0922b2e57f349687bb
Maia Hoberman <maiayh@umich.edu>
2015-02-22 01:56:38 -0500

    Worked on layour some more, as well as locking/unlocking

--- Assets/_Maia Assets/OpenThings.cs
+++ Assets/_Maia Assets/OpenThings.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class OpenThings : MonoBehaviour {
 	public Animator anim;
+	public bool willKill;
+	public bool holdsPasscard;
+	public bool needsPasscard;
+	public bool playerGotPasscard = false;
 	
 	void Awake () {
 		anim = GetComponent<Animator>();
 	}
 		
 	public void Interact () {
-		//print("registered click");
+		if (willKill) {/*do stuff*/}
+		if (needsPasscard && !playerGotPasscard) {
+			//play sound
+			return;
+		}
+		if (holdsPasscard) {
+			playerGotPasscard = true;
+			//UI stuff??
+		}
 		anim.SetBool("isOpen", !anim.GetBool("isOpen"));
 	}
 }
