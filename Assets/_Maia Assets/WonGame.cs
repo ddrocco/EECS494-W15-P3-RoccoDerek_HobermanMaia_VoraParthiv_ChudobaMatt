2405dbf851f7e58464da7fdc7c6310425399f696
Maia Hoberman <maiayh@umich.edu>
2015-02-22 02:24:02 -0500

    finishing up for the night

---
+++ Assets/_Maia Assets/WonGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WonGame : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if  (other.gameObject.layer == Layerdefs.player) {
+			//endgame
+		}
+	}
+}
