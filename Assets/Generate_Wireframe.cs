c5ff42cc4e111064843c3dcf2a951456a1c77dd9
ddrocco <keredoccor@gmail.com>
2015-02-23 00:19:38 -0500

    Wireframes are operational!

--- Assets/Generate_Wireframe.cs
+++ Assets/Generate_Wireframe.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class Generate_Wireframe : MonoBehaviour {
 	public GameObject wireframePrefab;
+	public int type = 0;
 	
 	void Start () {
 		GameObject wireframe = Instantiate(wireframePrefab, transform.position, Quaternion.identity) as GameObject;
 		wireframe.transform.parent = transform;
 		wireframe.transform.localScale = Vector3.one;
+		if (type == 1) { //dangerous
+			wireframe.renderer.material.color = Color.red;
+		} else if (type == 2) { //good
+			wireframe.renderer.material.color = Color.cyan;
+		}
 	}
 }
