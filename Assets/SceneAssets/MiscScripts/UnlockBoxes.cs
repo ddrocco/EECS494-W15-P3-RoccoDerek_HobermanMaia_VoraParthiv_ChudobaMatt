59ee2c19c9a7cecb1e168e86e3425a4b011f23b8
Maia Hoberman <maiayh@umich.edu>
2015-03-18 06:04:22 -0400

    Stuff I can save

---
+++ Assets/SceneAssets/MiscScripts/UnlockBoxes.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnlockBoxes : MonoBehaviour {
+	private static List<GameObject> boxes;
+
+	void Awake () {
+		boxes = new List<GameObject>();
+		GameObject[] objs = GameObject.FindGameObjectsWithTag("Interactive");
+		print ("gameobjs="+objs.Length);
+		foreach (GameObject obj in objs)
+		{
+			BoxControl temp = obj.GetComponentInChildren<BoxControl>();
+			if (temp != null) {
+				boxes.Add(obj);
+			}
+		}
+		print (boxes.Count);
+	}
+
+	public void UnlockAll() {
+		foreach (GameObject box in boxes) {
+			print ("onebox");
+			QInteractable interact = box.GetComponentInChildren<QInteractable>();
+			interact.enabled = true;
+		}
+	}
+}
