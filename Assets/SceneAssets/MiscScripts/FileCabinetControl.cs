c2c928a0957b266ba3ad936983d6c5fde394d7f6
Matt Chudoba <mchudoba5@gmail.com>
2015-03-18 14:10:02 -0400

    Tutorial text bugfix; removed Unity 4 backup

--- Assets/SceneAssets/MiscScripts/FileCabinetControl.cs
+++ Assets/SceneAssets/MiscScripts/FileCabinetControl.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class FileCabinetControl : MonoBehaviour {
 	public Animator anim;
 	public GameObject QCamera;
-	public string message;
+	private string message;
 	public string QMessage;
 	
 	void Awake () {
 		anim = GetComponent<Animator>();
+	}
+
+	void Start()
+	{
+		string control = "pressing (X)";
+		if (PlayerController.debugControls)
+			control = "right-clicking";
+		
+		message = "You can also tag objects if you want your " +
+			"partner to help you with them. Try tagging the " +
+				"camera in the corner by " + control + " when your targetter turns blue.";
 	}
 	
 	public void Interact () {
