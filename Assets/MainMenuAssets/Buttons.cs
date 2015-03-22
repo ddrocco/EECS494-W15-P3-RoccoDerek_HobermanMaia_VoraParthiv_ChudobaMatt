bf1048f9cfc4d2f15c47c0ebe28b00cfbcb820cb
Parthiv Vora <psvora@umich.edu>
2015-03-22 19:29:36 -0400

    Added the main menu
    Play works, not sure what "controls" should do yet.

---
+++ Assets/MainMenuAssets/Buttons.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	public void Play(){
+		Application.LoadLevel ("_Final_Prototype_Map");
+	}
+
+	public void Controls(){
+
+	}
+
+}
