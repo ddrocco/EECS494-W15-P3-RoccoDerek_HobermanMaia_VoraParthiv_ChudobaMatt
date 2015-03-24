cca8575ad8cb5f736c549440e38e0a11dcd4f337
ddrocco <keredoccor@gmail.com>
2015-03-24 03:46:32 -0400

    Stan visibility is now on by default

--- Assets/SceneAssets/_Q Assets/QPlayerVisibilityToggle.cs
+++ Assets/SceneAssets/_Q Assets/QPlayerVisibilityToggle.cs
@@ -12,9 +12,10 @@ public class QPlayerVisibilityToggle : MonoBehaviour {
 		arrowRenderer = stanRenderer.GetComponentInChildren<SpriteRenderer>();
 		arrowRenderer.enabled = false;
 		GetComponent<Image>().color = Color.gray;
+		ToggleVisibility();
 	}
 	
-	void Update () {
+	/*void Update () {
 		if (Input.GetKeyDown(KeyCode.P)) {
 			if (stanRenderer.enabled) {
 				stanRenderer.enabled = false;
@@ -25,6 +26,18 @@ public class QPlayerVisibilityToggle : MonoBehaviour {
 				arrowRenderer.enabled = true;
 				GetComponent<Image>().color = Color.green;
 			}
+		}
+	}*/
+	
+	void ToggleVisibility() {
+		if (stanRenderer.enabled) {
+			stanRenderer.enabled = false;
+			arrowRenderer.enabled = false;
+			GetComponent<Image>().color = Color.gray;
+		} else {
+			stanRenderer.enabled = true;
+			arrowRenderer.enabled = true;
+			GetComponent<Image>().color = Color.green;
 		}
 	}
 }
