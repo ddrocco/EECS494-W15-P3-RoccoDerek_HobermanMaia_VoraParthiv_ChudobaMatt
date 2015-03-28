c0fb17fa8ed6aa04aeed79c089a5591750d90fd6
Parthiv Vora <psvora@umich.edu>
2015-03-27 20:07:31 -0400

    Created a Prefab for the Pause Menu
    For some reason, the UI is broken on Stan's side.

---
+++ Assets/PauseMenuAssets/PauseScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using InControl;
+
+public class PauseScript : MonoBehaviour {
+
+	private InputDevice device;
+	public GameObject QPauseMenu;
+	public GameObject StanPauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		QPauseMenu.GetComponent<Canvas> ().worldCamera = GameObject.Find ("QCamera").GetComponent<Camera>();
+		StanPauseMenu.GetComponent<Canvas> ().worldCamera = GameObject.Find ("PlayerCamera").GetComponent<Camera>();
+		Resume ();
+	}
+
+	void Awake() {
+		device = InputManager.ActiveDevice;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (InputManager.MenuWasPressed || Input.GetKeyUp (KeyCode.Escape)) {
+			if(QPauseMenu.activeSelf || StanPauseMenu.activeSelf){
+				Resume();
+			} else {
+				QPauseMenu.SetActive(true);
+				StanPauseMenu.SetActive(true);
+				Time.timeScale = 0;
+			}
+		}
+	}
+
+	public void Resume(){
+		QPauseMenu.SetActive(false);
+		StanPauseMenu.SetActive(false);
+		Time.timeScale = 1;
+	}
+
+	public void Restart(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void Quit(){
+		Application.LoadLevel ("MainMenu");
+	}
+}
