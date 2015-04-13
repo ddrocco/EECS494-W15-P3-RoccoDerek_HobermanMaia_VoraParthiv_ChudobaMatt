0c46051e3374e92a3289e2b297bf3a4904daf2e0
Maia Hoberman <maiayh@umich.edu>
2015-04-12 22:42:50 -0400

    Working on L2 and bombs

---
+++ Assets/ExplosionControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionControl : MonoBehaviour {
+	public static ExplosionControl Instance;
+	public ParticleSystem smoke;
+	public ParticleSystem fire;
+	
+	void Awake() {
+		if (Instance != null) {
+			Debug.LogError ("Multiple instances of ExplosionControl!");
+		}
+		Instance = this;
+	}
+	
+	public void Explosion(Vector3 position) {
+		instantiate (smoke, position);
+		instantiate (fire, position);
+	}
+	
+	private ParticleSystem instantiate(ParticleSystem prefab, Vector3 pos) {
+		ParticleSystem newSystem = Instantiate(prefab, pos,
+		                                       Quaternion.identity)
+		                                       as ParticleSystem;
+		Destroy (newSystem.gameObject, newSystem.startLifetime);
+		return newSystem;
+	}
+}
