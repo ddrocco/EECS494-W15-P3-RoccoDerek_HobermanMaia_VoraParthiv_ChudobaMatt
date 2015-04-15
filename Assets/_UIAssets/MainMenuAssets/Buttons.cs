f5b4b37fec8af5549b27f3ddc9b3134eb2ed893a
Parthiv Vora <psvora@umich.edu>
2015-04-14 23:02:08 -0400

    Added Level Warp and new Build Settings

--- Assets/_UIAssets/MainMenuAssets/Buttons.cs
+++ Assets/_UIAssets/MainMenuAssets/Buttons.cs
@@ -4,8 +4,25 @@ using InControl;
 
 public class Buttons : MonoBehaviour {
 
+	public GameObject levelwarp;
+
 	public void Play(){
-		Application.LoadLevel ("_BetaLevel1");
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void LevelSelect(){
+		levelwarp.SetActive (true);
+	}
+
+	public void Back(){
+		levelwarp.SetActive (false);
+	}
+
+	public void LevelButton(int i){
+		if (Application.levelCount > i)
+			Application.LoadLevel (i);
+		else
+			Debug.Log ("No level with number " + i +" in Build Settings.");
 	}
 
 	void Update(){
