9f7f783e5576c97b2e21d30dbcbb0ab7335dbecb
Parthiv Vora <psvora@umich.edu>
2015-04-17 18:02:07 -0400

    Gave Stan level warp capability

--- Assets/_UIAssets/MainMenuAssets/Buttons.cs
+++ Assets/_UIAssets/MainMenuAssets/Buttons.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using InControl;
 
 public class Buttons : MonoBehaviour {
 
 	public GameObject levelwarp;
+	public GameObject levelwarpstan;
+	public GameObject stanlevels;
+	Button[] buttonset;
+	int stanbuttonindex = 0;
 
 	public void Play(){
 		Application.LoadLevel (Application.loadedLevel + 1);
@@ -26,7 +31,39 @@ public class Buttons : MonoBehaviour {
 	}
 
 	void Update(){
-		if (InputManager.MenuWasPressed) Play ();
+		//play from main menu
+		if (InputManager.MenuWasPressed && !levelwarpstan.activeInHierarchy) Play ();
+
+		//level select
+		if (Input.GetKeyDown (KeyCode.Y) && !levelwarpstan.activeInHierarchy) {
+			levelwarpstan.SetActive (true);
+			stanbuttonindex = 0;			
+			buttonset = stanlevels.GetComponentsInChildren<Button> ();
+			buttonset [stanbuttonindex].Select ();
+
+		} else if (levelwarpstan.activeInHierarchy) { //level select is up
+			//go back
+			if (Input.GetKeyDown(KeyCode.B)){
+				levelwarpstan.SetActive(false);
+			} 
+			//move right
+			else if (Input.GetKeyDown(KeyCode.RightArrow)){
+				if(stanbuttonindex + 1 < buttonset.Length){
+					buttonset[++stanbuttonindex].Select();
+				}
+			} 
+			//move left
+			else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+				if(stanbuttonindex - 1 >= 0){
+					buttonset[--stanbuttonindex].Select();
+				}
+			}
+			//select the level
+			else if(Input.GetKeyDown(KeyCode.A)){
+				LevelButton(stanbuttonindex + 1);
+			}
+		}
+
 	}
 
 }
