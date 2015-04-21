e275124a04ac0a3ace6317506a96e92f6924a5f6
Matt Chudoba <mchudoba5@gmail.com>
2015-04-21 01:58:40 -0400

    Ambient light given to Stan in L3

--- Team Spy/Assets/_WorldAssets/Lightpocalypse.cs
+++ Team Spy/Assets/_WorldAssets/Lightpocalypse.cs
@@ -3,10 +3,13 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Lightpocalypse : MonoBehaviour {
+	public Light stanLight;
+
 	void Interact() {
 		FindObjectOfType<BrokenLightParent>().Lightpocalypse();
 		foreach (GameObject text in GameObject.FindGameObjectsWithTag("EnableOnConnectText")) {
 			text.GetComponent<Text>().enabled = true;
 		}
+		stanLight.enabled = true;
 	}
 }
