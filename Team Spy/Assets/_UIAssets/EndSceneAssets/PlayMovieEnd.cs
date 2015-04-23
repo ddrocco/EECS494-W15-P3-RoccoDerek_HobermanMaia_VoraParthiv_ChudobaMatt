ae6909f3b467c27a7dd7d002bb56a6a253422b8d
Parthiv Vora <psvora@umich.edu>
2015-04-23 00:58:33 -0400

    Added the Ending Cutscene

---
+++ Team Spy/Assets/_UIAssets/EndSceneAssets/PlayMovieEnd.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayMovieEnd : MonoBehaviour {
+
+	InputDevice device;
+	MovieTexture movie;
+
+
+	// Use this for initialization
+	void Awake () {
+		device = InputManager.ActiveDevice;
+
+		movie = (MovieTexture)GetComponent<Renderer> ().material.mainTexture;
+		movie.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!movie.isPlaying
+			|| InputManager.MenuWasPressed
+			|| Input.GetKey (KeyCode.Escape) || Input.GetKey (KeyCode.Return) || Input.GetKey (KeyCode.KeypadEnter))
+
+			NextLevel ();
+	}
+
+	void NextLevel(){
+		if (Application.levelCount > Application.loadedLevel + 1)
+			Application.LoadLevel (Application.loadedLevel + 1);
+		else
+			Application.LoadLevel (0);
+	}
+}
