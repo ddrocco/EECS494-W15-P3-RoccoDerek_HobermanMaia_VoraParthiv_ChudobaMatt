9f2fb50cd69deadef5971023ab0817f3d646ce72
ddrocco <keredoccor@gmail.com>
2015-04-23 10:18:33 -0400

    Made some modifications to chairs so they don't fall over so easily

---
+++ Team Spy/Assets/_WorldAssets/SceneBuildingObjects/ChairStabilityAssistance.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChairStabilityAssistance : MonoBehaviour {
+	Rigidbody rigid;
+	
+	void Start() {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		rigid.AddForce(20f * Vector3.down);
+	}
+}
