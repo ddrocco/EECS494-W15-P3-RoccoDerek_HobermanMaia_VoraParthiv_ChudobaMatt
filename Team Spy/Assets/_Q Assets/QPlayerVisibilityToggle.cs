aedfade911d912f7edf545286f3b6a527b8f41ea
Maia Hoberman <maiayh@umich.edu>
2015-04-24 02:49:41 -0400

    moving/reorganizing.

--- Team Spy/Assets/_Q Assets/QPlayerVisibilityToggle.cs
+++ Team Spy/Assets/_Q Assets/QPlayerVisibilityToggle.cs
@@ -15,20 +15,6 @@ public class QPlayerVisibilityToggle : MonoBehaviour {
 		ToggleVisibility();
 	}
 	
-	/*void Update () {
-		if (Input.GetKeyDown(KeyCode.P)) {
-			if (stanRenderer.enabled) {
-				stanRenderer.enabled = false;
-				arrowRenderer.enabled = false;
-				GetComponent<Image>().color = Color.gray;
-			} else {
-				stanRenderer.enabled = true;
-				arrowRenderer.enabled = true;
-				GetComponent<Image>().color = Color.green;
-			}
-		}
-	}*/
-	
 	void ToggleVisibility() {
 		if (stanRenderer.enabled) {
 			stanRenderer.enabled = false;
